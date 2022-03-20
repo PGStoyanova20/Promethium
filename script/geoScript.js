d7a6d561987c07cc581b8360e0b312db2943c5a1
PGStoyanova20 <PGStoyanova20@codingburgas.bg>
2022-03-20 21:12:12 +0200

    Add comments to .js files

--- script/geoScript.js
+++ script/geoScript.js
@@ -1,30 +1,22 @@
-var map = L.map('map').setView([42.35, 26.19], 8);
+//Create map
+var map = L.map('map').setView([42.35, 26.19], 7);
     
-    var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1
-    }).addTo(map);
+var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
 
-    var marker = L.marker([43.227556, 27.706358]).addTo(map)
-        .bindPopup('<b>The Stone Forest</b>').openPopup();
+//Create popups
+var marker = L.marker([43.227556, 27.706358]).addTo(map)
+    .bindPopup('<b>The Stone Forest</b>').openPopup();
 
-    var marker = L.marker([43.175, 24.073]).addTo(map)
-        .bindPopup('<b>Prohodna Cave</b>').openPopup();
+var marker = L.marker([43.175, 24.073]).addTo(map)
+    .bindPopup('<b>Prohodna Cave</b>').openPopup();
         
-    var marker = L.marker([43.243578, 25.033369]).addTo(map)
-        .bindPopup('<b>Krushuna Waterfalls</b>').openPopup();
+var marker = L.marker([43.243578, 25.033369]).addTo(map)
+    .bindPopup('<b>Krushuna Waterfalls</b>').openPopup();
 
-    
-
-    function onMapClick(e) {
-        popup
-            .setLatLng(e.latlng)
-            .setContent('You clicked the map at ' + e.latlng.toString())
-            .openOn(map);
-    }
-
-    map.on('click', onMapClick);
--- script/hisScript.js
+++ script/hisScript.js
@@ -1,30 +1,22 @@
-var map = L.map('map').setView([42.35, 26.19], 8);
+//Create map
+var map = L.map('map').setView([41.35, 23.19], 7);
     
-    var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1
-    }).addTo(map);
+var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
 
-    var marker = L.marker([41.524605, 23.391510]).addTo(map)
-        .bindPopup('<b>Melnik</b>').openPopup();
+//Create popups
+var marker = L.marker([41.524605, 23.391510]).addTo(map)
+    .bindPopup('<b>Melnik</b>').openPopup();
 
-    var marker = L.marker([43.075672, 25.617151]).addTo(map)
-        .bindPopup('<b>Veliko Tarnovo</b>').openPopup();
+var marker = L.marker([43.075672, 25.617151]).addTo(map)
+    .bindPopup('<b>Veliko Tarnovo</b>').openPopup();
         
-    var marker = L.marker([43.99, 22.8725]).addTo(map)
-        .bindPopup('<b>Vidin</b>').openPopup();
+var marker = L.marker([43.99, 22.8725]).addTo(map)
+    .bindPopup('<b>Vidin</b>').openPopup();
 
-    
-
-    function onMapClick(e) {
-        popup
-            .setLatLng(e.latlng)
-            .setContent('You clicked the map at ' + e.latlng.toString())
-            .openOn(map);
-    }
-
-    map.on('click', onMapClick);
